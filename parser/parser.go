4b9f4a8826d0ba8768b16c3b9b99ff6732e942da
Semenchenko Kirill <semenchenkoke@gmail.com>
2019-02-25 10:01:50 +0300

    some example

--- parser/parser.go
+++ parser/parser.go
@@ -13,30 +13,39 @@ var reStock = regexp.MustCompile(`(?m)(\d+) in stock`)
 
 func ParceCardsInDoc(doc *goquery.Document) ([]goscgp.Card, error) {
 	var cards []goscgp.Card
+	var breakErr error
 
-	doc.Find("#content > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td").Each(func(i int, s *goquery.Selection) {
-		var card goscgp.Card
-		card.Name = s.Find("h2").Text()
-		card.Set = strings.TrimSuffix(s.Find("div > div.card_desc_details > div:nth-child(1) > div:nth-child(1) > h3 > a").Text(), "\n")
-		s.Find("div > div:nth-child(2) > div:nth-child(2) > span").Each(func(i int, s *goquery.Selection) {
-			var con goscgp.Condition
+	doc.Find("#content > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td").EachWithBreak(
+		func(i int, s *goquery.Selection) bool {
+			var card goscgp.Card
+			card.Name = s.Find("h2").Text()
+			card.Set = strings.TrimSuffix(s.Find("div > div.card_desc_details > div:nth-child(1) > div:nth-child(1) > h3 > a").Text(), "\n")
 
-			con.Condition = s.Find("a").Text()[0:2]
-			con.Price = s.Find("div:nth-child(4)").Text()
-			if reStock.FindStringSubmatch(s.Text()) != nil {
-				var err error
-				con.Count, err = strconv.Atoi(reStock.FindStringSubmatch(s.Text())[1])
-				if err != nil {
-					// FIXME: Should return error
+			s.Find("div > div:nth-child(2) > div:nth-child(2) > span").EachWithBreak(func(i int, s *goquery.Selection) bool {
+				var con goscgp.Condition
+
+				con.Condition = s.Find("a").Text()[0:2]
+				con.Price = s.Find("div:nth-child(4)").Text()
+				if reStock.FindStringSubmatch(s.Text()) != nil {
+					var err error
+					con.Count, err = strconv.Atoi(reStock.FindStringSubmatch(s.Text())[1])
+					if err != nil {
+						con.Count = 0
+						breakErr = err
+						return false
+					}
+				} else {
 					con.Count = 0
 				}
-			} else {
-				con.Count = 0
+				card.Conditions = append(card.Conditions, con)
+				return true
+			})
+			if breakErr != nil {
+				return false
 			}
-			card.Conditions = append(card.Conditions, con)
+			cards = append(cards, card)
+			return true
 		})
-		cards = append(cards, card)
-	})
 
-	return cards, nil
+	return cards, breakErr
 }
