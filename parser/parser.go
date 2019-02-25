b24b2c441c31b68c8c7a8e55f40cb968054cbdcf
Aleksey Sviridkin <f@lex.la>
2019-02-25 07:21:03 +0300

    Update foil and price in parse func

--- parser/parser.go
+++ parser/parser.go
@@ -10,6 +10,7 @@ import (
 )
 
 var reStock = regexp.MustCompile(`(?m)(\d+) in stock`)
+var reNonDigit = regexp.MustCompile(`(?m)\D`)
 
 func ParceCardsInDoc(doc *goquery.Document) ([]goscgp.Card, error) {
 	var cards []goscgp.Card
@@ -17,12 +18,16 @@ func ParceCardsInDoc(doc *goquery.Document) ([]goscgp.Card, error) {
 	doc.Find("#content > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td").Each(func(i int, s *goquery.Selection) {
 		var card goscgp.Card
 		card.Name = s.Find("h2").Text()
-		card.Set = strings.TrimSuffix(s.Find("div > div.card_desc_details > div:nth-child(1) > div:nth-child(1) > h3 > a").Text(), "\n")
+		set := strings.TrimSuffix(s.Find("div > div.card_desc_details > div:nth-child(1) > div:nth-child(1) > h3 > a").Text(), "\n")
+		if strings.Contains(set, " (Foil)") {
+			card.Foil = true
+			set = strings.TrimSuffix(set, " (Foil)")
+		}
+		card.Set = set
 		s.Find("div > div:nth-child(2) > div:nth-child(2) > span").Each(func(i int, s *goquery.Selection) {
 			var con goscgp.Condition
-
 			con.Condition = s.Find("a").Text()[0:2]
-			con.Price = s.Find("div:nth-child(4)").Text()
+			con.Price = reNonDigit.ReplaceAllString(s.Find("div:nth-child(4)").Text(), "")
 			if reStock.FindStringSubmatch(s.Text()) != nil {
 				var err error
 				con.Count, err = strconv.Atoi(reStock.FindStringSubmatch(s.Text())[1])
